b0432ebfed3cc34c75c655d307c158fc27220421
Helena <4neeb@informatik.uni-hamburg.de>
2015-05-23 17:36:04 +0200

    test

---
+++ Test/src/Converter.java
@@ -0,0 +1,23 @@
+class Converter {
+
+	public static String intToString(int x)
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		String sign = "";
+		if (x < 0)
+		{
+			sign = ".";
+			x = -x;
+		}
+		
+	while (x != 0)
+	{
+		sb.append((char)('0' + x % 10));
+		x = x / 10;
+	}
+	
+	return sb.insert(0, sign).toString();
+	}
+
+}
